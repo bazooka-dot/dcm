0fecfe1734ed2b7883519544f878a533150e7f44
Azili Youssef <152706418+bazooka-dot@users.noreply.github.com>
2025-08-20 01:12:22 +0100

    Initial commit

---
+++ DCMapplication/src/test/java/com/example/DCMapplication/DcMapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DCMapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcMapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
